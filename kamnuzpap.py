d1035502a48dba21090e5a9467d9f6d023b86ab5
Karolina Kamenicka <karolina@kamenicka.com>
2020-04-22 20:15:44 +0200

    ukonceni od uzivatele

--- kamnuzpap.py
+++ kamnuzpap.py
@@ -1,22 +1,25 @@
 #kamnuzpap
 #autor: karolina
 from random import randrange
+
 vstup = input('Zadej kamen, nuzky nebo papir: ')
-cislo = randrange(3)
-if cislo == 0:
-    pocitac = 'kamen'
-elif cislo == 1:
-    pocitac = 'nuzky'
-else: 
-    pocitac = 'papir'
+while vstup != 'konec':
+    cislo = randrange(3)
+    if cislo == 0:
+      pocitac = 'kamen'
+    elif cislo == 1:
+      pocitac = 'nuzky'
+    else: 
+        pocitac = 'papir'
 
-print('Pocitac zadal: ' + pocitac)
+    print('Pocitac zadal: ' + pocitac)
 
-if vstup == pocitac:
-    vysledek = 'plichta'
-elif (vstup == 'kamen' and pocitac == 'nuzky') or (vstup == 'nuzky' and pocitac =='papir') or (vstup =='papir' and pocitac =='kamen'):
-    vysledek = 'vyhral jsi'
-else :
-    vysledek = 'vyhral pocitac'
-print('Vysledek zapasu je: ' +vysledek)
+    if vstup == pocitac:
+      vysledek = 'plichta'
+    elif (vstup == 'kamen' and pocitac == 'nuzky') or (vstup == 'nuzky' and pocitac =='papir') or (vstup =='papir' and pocitac =='kamen'):
+       vysledek = 'vyhral jsi'
+    else :
+       vysledek = 'vyhral pocitac'
+    print('Vysledek zapasu je: ' +vysledek)
+    vstup = input('Zadej kamen, nuzky nebo papir: ')
 
