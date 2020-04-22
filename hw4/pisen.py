d728225cec6be9e0c3a69e11738c545b1b336b84
Karolina Kamenicka <karolina@kamenicka.com>
2020-04-22 20:26:20 +0200

    create

---
+++ hw4/pisen.py
@@ -0,0 +1,5 @@
+text = 'Youre a troublemaker Youre a troublemaker You aint nothing but a troublemaker girlYou had me hooked again from the minute you sat down'
+
+number = text.find('k')
+
+print('obsahuje to pismeno k ' + str(number) + ' krat.')
